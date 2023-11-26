73fb7f08b98b9e837ab9300c4ce30864a0b590f7
iGeek <servicesonlines139@gmail.com>
2023-11-26 16:35:21 +0000

    EXERCICE_2

---
+++ EXO_2.php
@@ -0,0 +1,10 @@
+<?php
+$nombre = 19;
+if ($nombre % 3 == 0 && $nombre % 5 == 0 ) 
+    {
+        echo "$nombre est un multiple de 3 et de 5";
+    }
+else{
+    echo "$nombre n'est pas un multiple de 3 et de 5";
+}
+?>
